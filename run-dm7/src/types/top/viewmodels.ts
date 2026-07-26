9596330af0bd128b86ec119636b1760b6d95b0a3
2MRW-USIsan <get2mrw.cowboy@gmail.com>
2026-07-27 00:13:37 +0900

    Add Types Files

--- run-dm7/src/types/top/viewmodels.ts
+++ run-dm7/src/types/top/viewmodels.ts
@@ -1,4 +1,8 @@
 import * as Comps from "@/types/top/components";
+import * as Props from "@/types/top/properties";
+import * as Hndls from "@/types/top/handlers";
+import * as Rdcrs from "@/types/top/reducers";
+import * as Srvcs from "@/types/top/services";
 
 export type TopViewModels = Comps.TopPageComps;
 
@@ -14,10 +18,40 @@ export type PresetComposed = {};
 export type NotionComposed = {};
 export type EditorComposed = {};
 
-export type TopProperty = {};
-export type TopHandlers = {};
+export type TopProperty = {
+  page: Props.TopPageProperty;
+  preset: Props.PresetProperty;
+  notion: Props.NotionProperty;
+  editor: Props.EditorProperty;
+};
+export type TopHandlers = {
+  page: Hndls.TopPageHandlers;
+  preset: Hndls.PresetHandlers;
+  notion: Hndls.NotionHandlers;
+  editor: Hndls.EditorHandlers;
+};
 
-export type TopContexts = {};
+export type TopContexts = {
+  page: TopPageContexts;
+  preset: PresetContexts;
+  notion: NotionContexts;
+  editor: EditorContexts;
+};
 
-export type TopReducers = {};
-export type TopServices = {};
+export type TopPageContexts = {};
+export type PresetContexts = {};
+export type NotionContexts = {};
+export type EditorContexts = {};
+
+export type TopReducers = {
+  page: Rdcrs.TopPageReducers;
+  preset: Rdcrs.PresetReducers;
+  notion: Rdcrs.NotionReducers;
+  editor: Rdcrs.EditorReducers;
+};
+export type TopServices = {
+  page: Srvcs.TopPageServices;
+  preset: Srvcs.PresetServices;
+  notion: Srvcs.NotionServices;
+  editor: Srvcs.EditorServices;
+};
